e68a1938b285bedd92ecdbd3bc076e94fc5215dc
cslunetta <cslunetta@outlook.com>
2021-02-02 11:10:27 -0600

    update settings and install React Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import $ from "jquery";
+// import Popper from "popper.js"
+// import "bootstrap/dist/js/bootstrap.bundle.min"
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./components/App.js";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
