cff0f9fe89ce1b7d838e826580332b540ec2edb4
Christopher <72471899+cslunetta@users.noreply.github.com>
2021-02-03 13:40:04 -0600

    remove comments from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// import "bootstrap/dist/css/bootstrap.min.css"
-// import $ from "jquery";
-// import Popper from "popper.js"
-// import "bootstrap/dist/js/bootstrap.bundle.min"
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
