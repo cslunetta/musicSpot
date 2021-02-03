b5015cffcfda3ab7ee0778550e3b5f59f519f036
cslunetta <cslunetta@outlook.com>
2021-02-02 23:23:11 -0600

    corrected EventsProvider and EventList to match how api will represent data

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { EventContext } from "./EventsProvider";
 import { EventCard } from "./EventCard";
-import "./event.css"
+import "./event.css";
 
 export const EventList = () => {
   const { events, getEvents } = useContext(EventContext);
@@ -15,8 +15,8 @@ export const EventList = () => {
     <>
       <h2>Events</h2>
       <div className="events">
-        {console.log("EventList: Render", events.events)}
-        {events.events?.map((event) => {
+        {console.log("EventList: Render", events)}
+        {events._embedded?.events?.map((event) => {
           return <EventCard key={event.id} event={event} />;
         })}
       </div>
--- src/components/events/EventsProvider.js
+++ src/components/events/EventsProvider.js
@@ -7,18 +7,10 @@ export const EventProvider = (props) => {
   const [events, setEvents] = useState([]);
 
   const getEvents = () => {
-    return fetch("http://localhost:8088/_embedded?=events")
+    return fetch("http://localhost:8088/events")
       .then((res) => res.json())
       .then(setEvents);
   };
-
-  // const getEvents = () => {
-  //   return fetch(
-  //     `https://app.ticketmaster.com/discovery/v2/events?apikey=${apikey}&locale=*&startDateTime=2021-01-26T14:17:00Z&endDateTime=2021-02-26T14:17:00Z&sort=date,name,asc&city=Nashville&stateCode=tn&segmentId=KZFzniwnSyZfZ7v7nJ`
-  //   )
-  //     .then((res) => res.json())
-  //     .then(setEvents);
-  // };
 
   return (
     <EventContext.Provider
@@ -31,3 +23,4 @@ export const EventProvider = (props) => {
     </EventContext.Provider>
   );
 };
+
